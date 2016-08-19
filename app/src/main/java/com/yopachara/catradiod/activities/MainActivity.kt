4b1a85627930ffc9b63c5b811506194c6cec23df
Pachara Sukcharoen <pachara@dezainwerkz.com>
2016-08-19 17:04:52 +0700

    Add SlidingUpPanelLayout and fabric prepare for twitter

--- app/src/main/java/com/yopachara/catradiod/activities/MainActivity.kt
+++ app/src/main/java/com/yopachara/catradiod/activities/MainActivity.kt
@@ -1,21 +1,18 @@
-package com.yopachara.catradiod
+package com.yopachara.catradiod.activities
 
 import android.net.Network
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import com.yopachara.catradiod.R
 import kotlinx.android.synthetic.main.activity_main.*
 import com.yopachara.catradiod.libraries.radio.RadioListener
 import com.yopachara.catradiod.libraries.radio.RadioManager
-import com.yopachara.catradiod.services.CatService
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity(), RadioListener {
     private val RADIO_URL: String = "http://catradio.mylive.in.th:8000/live"
-
     internal var mRadioManager: RadioManager = RadioManager.with(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +22,15 @@ class MainActivity : AppCompatActivity(), RadioListener {
         mRadioManager.setLogging(true)
         buttonControlStart.setOnClickListener { view -> initializeUI() }
 
+        slidingLayout.addPanelSlideListener(object : SlidingUpPanelLayout.PanelSlideListener {
+            override fun onPanelSlide(panel: View, slideOffset: Float) {
+                Log.i("onPanelSlide", "onPanelSlide, offset " + slideOffset)
+            }
 
+            override fun onPanelStateChanged(panel: View, previousState: SlidingUpPanelLayout.PanelState, newState: SlidingUpPanelLayout.PanelState) {
+                Log.i("onPanelStateChanged", "onPanelStateChanged " + newState)
+            }
+        })
 
 
     }
@@ -75,7 +80,7 @@ class MainActivity : AppCompatActivity(), RadioListener {
 
     override fun onMetaDataReceived(s: String, s1: String) {
         //TODO Check metadata values. Singer name, song name or whatever you have.
-        Log.d(s,s1)
+        Log.d(s, s1)
     }
 
     override fun onError() {
